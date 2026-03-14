b3806965b843ca8b6803b9d407776952ffb0dcd7
odonML <odon_ml@outlook.com>
2026-03-14 13:23:27 -0600

    bash: delimiter print block code in terminal

---
+++ 03-delimiter.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# el delimitador es una palabra que se usa para indicar el inicio y el fin de un bloque de texto
+# el bloque de texto se muestra en la terminal
+cat << myText
+este texto 
+se mostrara en
+la terminal
+myText
