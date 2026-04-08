69a13b1f0238181915912aa04e965ab765131dd9
odonML <odon_ml@outlook.com>
2026-04-07 23:19:11 -0600

    bash: example, script with arguments

---
+++ 13-argumentos.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Este script muestra cómo usar argumentos en un script de bash
+echo "El primer argumento es: $1"
+echo "El segundo argumento es: $2"
+echo "Todos los argumentos son: $@"
+echo "El número de argumentos es: $#"
